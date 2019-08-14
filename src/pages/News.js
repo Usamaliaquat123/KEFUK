6b8242eba8c656a00bcf86e05be95d2316c34e36
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-08-14 13:59:25 -0700

    change

--- src/pages/News.js
+++ src/pages/News.js
@@ -6,11 +6,6 @@ import NavigatorView from "../components/NavigatorView/NavigatorView"
 import NewsEventsSlideOne from "../components/News&events/Slide1/NewsEventsSlideOne"
 import NewsEventsSlideThree from "../components/News&events/Slide3/NewsEventsSlideThree"
 import NewsEventsSlideTwo from "../components/News&events/Slide2/NewsEventsSlideTwo"
-// import Donation from "../components/donaion/donation"
-// import Subscribe from "../components/Subscribe/Subscribe"
-// import BottomNav from "../components/bottomNav"
-// import BasicFeatures from "../components/basicFeatures/basicFeatures"
-// import WhereWeWorkSlideOne from "../components/WhereWeWork/svps/Slide1/WhereWeWorkSlideOne"
 
 export class News extends Component {
   static propTypes = {}

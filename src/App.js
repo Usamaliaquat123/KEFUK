34ed002dd62bfe0a6a04f530e3bbe00f479b595e
Usama liaquat <inconnent12345@outlook.com>
2019-09-05 22:48:49 -0700

    sds

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // import PaymentApp from './conponents/payment/Payment';
-import Layout from "./container/Layout";
 import AppRouter from "./routing/router";
 function App() {
   return (
